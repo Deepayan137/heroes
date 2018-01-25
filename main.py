c90fe63b0f64345c3217bd020eeeed9befc0e941
Avijit <avijitdasgupta9@gmail.com>
2018-01-25 23:29:39 +0530

    training and tetsing both added

--- main.py
+++ main.py
@@ -22,22 +22,28 @@ if __name__ == '__main__':
 	test_1 = params["test_1"]
 	test_9 = params["test_9"]
 	hero_data = params["hero_data"]
-	X_train, y_train, X_val, uid = read_data(test_1, test_9, hero_data, test=1)
+	X_test, y_test, X_val, uid = read_data(test_1, test_9, hero_data, test=1)
+	X_train, y_train, X_val, y_val, uid = read_data(test_1, test_9, hero_data, test=1)
 	
-	regr_linear = linear_model.LinearRegression()
+	# Model Creation
 	print('processing GridSearch')
 	parameters = {"max_depth": [2,3,4,5,6,7,8,9,10,11,12],"min_samples_split" :[2,3,4,5,6] ,"n_estimators" : [10]    ,"min_samples_leaf": [1,2,3,4,5]    ,"max_features": (2,3,4)}
 	rf_regr = RandomForestRegressor()
 	model = GridSearchCV(rf_regr,parameters, n_jobs = 3, cv = 10)
-	# pdb.set_trace()
+
+	# Va;idating on the training set
 	model.fit(X_train, y_train)
 	print("Best parameters found by grid search:")
 	print(model.best_params_)
 	print("Best CV score:")
 	print(model.best_score_)
-	
-	# regr_linear.fit(X_train, y_train)
+	y_pred_val = model.predict(X_val)	
 
+	print("Mean squared error: %.2f"
+      % mean_squared_error(y_val,y_pred_val))
+	
+	# Finally the test dataset
+	model.fit(X_test, y_test)
 	y_pred = model.predict(X_val)
 	# y_pred_linear = regr_linear.predict(X_val)
 	# final_pred = (y_pred+y_pred_linear)/2
@@ -49,5 +55,4 @@ if __name__ == '__main__':
 		df.to_csv("submission.csv", index=False)
 	submission()
 
-	# print("Mean squared error: %.2f"
- #      % mean_squared_error(y_val,final_pred))
+
