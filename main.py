e1978bd8f8be730d2f5ebf60055da474d0fafc53
Deepayan <deepayan137@gmail.com>
2018-01-25 22:50:13 +0530

    added option for test files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import datasets, linear_model
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from utils.parser import params 
 from utils.preproc import read_data
@@ -9,17 +10,24 @@ import pandas as pd
 if __name__ == '__main__':
 	train_1 = params["train_1"]
 	train_9 = params["train_9"]
+	test_1 = params["test_1"]
+	test_9 = params["test_9"]
 	hero_data = params["hero_data"]
-	X_train, y_train, X_val, y_val = read_data(train_1, train_9, hero_data)
-	# pdb.set_trace()
-	regr = linear_model.LinearRegression()
+	X_train, y_train, X_val, = read_data(test_1, test_9, hero_data, test=1)
+	
+	regr_linear = linear_model.LinearRegression()
+	regr = RandomForestRegressor(n_estimators=100, max_depth=19)
 	regr.fit(X_train, y_train)
+	regr_linear.fit(X_train, y_train)
 	y_pred = regr.predict(X_val)
+	y_pred_linear = regr_linear.predict(X_val)
+	final_pred = (y_pred+y_pred_linear)/2
+	
 	def submission():
-		data = [[X_val["id"].iloc[i], y_pred[i]] for i in range(len(y_pred))]
+		data = [[X_val["id"].iloc[i], final_pred[i]] for i in range(len(final_pred))]
 		df = pd.DataFrame(data, columns=["id", "kda_ratio"])
 		df.to_csv("submission.csv", index=False)
 	submission()
-	
-	print("Mean squared error: %.2f"
-      % mean_squared_error(y_val,y_pred))
+
+	# print("Mean squared error: %.2f"
+ #      % mean_squared_error(y_val,final_pred))
--- utils/preproc.py
+++ utils/preproc.py
@@ -8,26 +8,38 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import LabelEncoder
 import pdb
 lb_make = LabelEncoder()
-def read_data(train_1, train_9, hero_data):
+
+
+def read_data(train_1, train_9, hero_data, test=None):
 	df_9 = pd.read_csv(train_9)
 	df_1 = pd.read_csv(train_1)
 	df_hero = pd.read_csv(hero_data)
-	df_1["win_ratio"] = df_1["num_wins"]/df_1["num_games"]
-	df_9["win_ratio"] = df_9["num_wins"]/df_9["num_games"]
 	
-	
-	merged_9 = df_9.merge(df_hero, on = 'hero_id', how='left')
-	merged_9["primary_attr"] = lb_make.fit_transform(merged_9["primary_attr"])
-	merged_9["attack_type"] = lb_make.fit_transform(merged_9["attack_type"])
-	merged_9["roles"] = lb_make.fit_transform(merged_9["roles"])
-	
-	merged_1 = df_1.merge(df_hero, on = 'hero_id', how='left')
-	merged_1["primary_attr"] = lb_make.fit_transform(merged_1["primary_attr"])
-	merged_1["attack_type"] = lb_make.fit_transform(merged_1["attack_type"])
-	merged_1["roles"] = lb_make.fit_transform(merged_1["roles"])
-	
-	
+	def transform(df, **kwargs):
+		attributes = kwargs["attributes"]
+		for attribute in attributes:
+			df[attribute] = lb_make.fit_transform(df[attribute])
+		return df
 
+	if test != None:
+
+		df_9 = pd.read_csv(train_9)
+		df_1 = pd.read_csv(train_1)
+		merged_9 = df_9.merge(df_hero, on = 'hero_id', how='left')
+		merged_1 = df_1.merge(df_hero, on = 'hero_id', how='left')
+		merged_9 = transform(merged_9, attributes=["primary_attr", "attack_type","roles"])
+		merged_1 = transform(merged_1, attributes=["primary_attr", "attack_type","roles"])
+		y_train = merged_9["kda_ratio"]
+		X_train = merged_9.drop(["kda_ratio", "hero_id", "base_health", "num_wins"], axis=1)
+		X_test = merged_1.drop(["hero_id", "base_health"], axis=1)
+		return X_train, y_train, X_test
+
+	merged_9 = df_9.merge(df_hero, on = 'hero_id', how='left')
+	merged_1 = df_1.merge(df_hero, on = 'hero_id', how='left')
+	merged_9 = transform(merged_9, attributes=["primary_attr", "attack_type","roles"])
+	merged_1 = transform(merged_1, attributes=["primary_attr", "attack_type","roles"])
+	merged_1["win_ratio"] = merged_1["num_wins"]/merged_1["num_games"]
+	merged_9["win_ratio"] = merged_9["num_wins"]/merged_9["num_games"]
 	y_train = merged_9["kda_ratio"]
 	y_val = merged_1["kda_ratio"]
 	X_train = merged_9.drop(["kda_ratio", "hero_id", "base_health"], axis=1)
